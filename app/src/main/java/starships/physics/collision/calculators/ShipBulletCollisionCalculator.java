2811c8f0ca5beff7e4fec7009f252dca58ce142e
ricardolopezb <ricardolopezbonaguro@gmail.com>
2022-11-23 18:38:18 -0300

    fixed shooting collision

--- app/src/main/java/starships/physics/collision/calculators/ShipBulletCollisionCalculator.java
+++ app/src/main/java/starships/physics/collision/calculators/ShipBulletCollisionCalculator.java
@@ -20,6 +20,8 @@ public class ShipBulletCollisionCalculator implements CollisionCalculator{
     @Override
     public GameEngine handleCollision(GameEngine currentState, String shipId, String bulletId) {
         System.out.println("Ship id = " + shipId + " bullet-id = " + bulletId);
+
+
         Optional<ShipController> ship = currentState.findShip(shipId);
         Optional<Mover> bulletMover = currentState.findMover(bulletId);
         List<String> newRemovedIds = new ArrayList<>(currentState.getRemovedIds());
@@ -29,6 +31,7 @@ public class ShipBulletCollisionCalculator implements CollisionCalculator{
 
         if(bulletMover.isPresent() && ship.isPresent()){
             Bullet bullet = (Bullet) bulletMover.get().getEntity();
+            if(shipId.equals(bullet.getOwnerId())) return currentState;
             Integer bulletDamage = bullet.getDamage();
             Optional<ShipController> damagedShip = ship.get().takeDamage(bulletDamage);
             if(damagedShip.isEmpty()){
